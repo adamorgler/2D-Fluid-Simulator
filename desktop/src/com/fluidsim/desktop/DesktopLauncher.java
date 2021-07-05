4177d78e59d3547b6498d0cf53abdb9ca7e58fd9
aorgler <aorgler@iastate.edu>
2021-07-05 17:40:04 -0500

    created project

---
+++ desktop/src/com/fluidsim/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fluidsim.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fluidsim.FluidSimuation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FluidSimuation(), config);
+	}
+}
